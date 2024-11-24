70bceae6dbea887071232015f3b8c7e74bd37a4b
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-11-23 22:58:13 -0400

    Add font awesome icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@headlessui/react";
 
 export default function Header() {
   return (
     <header className="bg-primary-gray-900 h-24 flex items-center justify-between p-4 shadow-lg">
       <div>Albion Raid Manager</div>
-      <Button>Login</Button>
+      <Button className="flex">
+        <FontAwesomeIcon icon={faDiscord} />
+        <span>Login</span>
+      </Button>
     </header>
   );
 }
