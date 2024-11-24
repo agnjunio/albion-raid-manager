29eb55d0df047beb0dbfb401040742fb52ee98ed
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-11-24 01:05:28 -0400

    Add link to guild list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@headlessui/react";
+import { useRouter } from "next/navigation";
+import React from "react";
 
-export default async function Home() {
+export default function Home() {
+  const router = useRouter();
+
+  const login: React.MouseEventHandler = (event) => {
+    event.preventDefault();
+    router.push("/guilds");
+  };
+
   return (
     <div className="h-full flex items-center justify-center">
-      <Button>
+      <Button onClick={login}>
         <FontAwesomeIcon icon={faDiscord} />
         Login
       </Button>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="bg-primary-gray-900 h-24 flex items-center justify-between p-4 shadow-lg">
-      <h1 className="text-3xl font-semibold font-sans">Albion Raid Manager</h1>
+      <Link href="/">
+        <h1 className="text-3xl font-semibold font-sans">Albion Raid Manager</h1>
+      </Link>
     </header>
   );
 }
