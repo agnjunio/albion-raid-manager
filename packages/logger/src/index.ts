120ad30c540a2b3a94324796a0bade0849a02693
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-11-28 05:42:39 -0400

    Disable logging to file in production

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -3,6 +3,7 @@ import path from "node:path";
 import { createLogger, format, transports } from "winston";
 
 const level: string = config.has("logger.level") ? config.get("logger.level") : "debug";
+const file: boolean = config.has("logger.file") ? config.get("logger.file") : true;
 
 const logger = createLogger({
   level: "debug",
@@ -15,7 +16,29 @@ const logger = createLogger({
       return process.env.SHARD;
     },
   },
-  transports: [
+});
+
+if (process.env.NODE_ENV !== "production") {
+  const consoleFormat = format.printf(({ level, [Symbol.for("level")]: logLevel, message, timestamp, shard }) => {
+    const printSpace = (count: number) => " ".repeat(Math.max(count, 0));
+
+    const maxLen = "verbose".length;
+    const count = maxLen - (logLevel as string).length;
+    const spacing = printSpace(count);
+    const shardStr = shard ? `[#${shard}] ` : "";
+    return `${timestamp} [${level}] ${spacing}: ${shardStr}${message}`;
+  });
+
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), format.colorize(), consoleFormat),
+      level,
+    }),
+  );
+}
+
+if (file) {
+  logger.add(
     new transports.File({
       filename: path.join("logs", "debug.log"),
       level: "debug",
@@ -23,31 +46,14 @@ const logger = createLogger({
       maxFiles: 1,
       tailable: true,
     }),
+  );
+  logger.add(
     new transports.File({
       filename: path.join("logs", "error.log"),
       level: "error",
       maxsize: 1 * Math.pow(1024, 1), // 1 MB
       maxFiles: 1,
       tailable: true,
-    }),
-  ],
-});
-
-const consoleFormat = format.printf(({ level, [Symbol.for("level")]: logLevel, message, timestamp, shard }) => {
-  const printSpace = (count: number) => " ".repeat(Math.max(count, 0));
-
-  const maxLen = "verbose".length;
-  const count = maxLen - (logLevel as string).length;
-  const spacing = printSpace(count);
-  const shardStr = shard ? `[#${shard}] ` : "";
-  return `${timestamp} [${level}] ${spacing}: ${shardStr}${message}`;
-});
-
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), format.colorize(), consoleFormat),
-      level,
     }),
   );
 }
