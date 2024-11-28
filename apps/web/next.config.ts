4f2eb604b0f01212c1a87ab542b98a6626727672
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-11-28 06:22:29 -0400

    Workaround for webpack prisma

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,15 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 import NodeConfigWebpack from "node-config-webpack";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+
     config.plugins.push(new NodeConfigWebpack());
     return config;
   },
---
+++ apps/web/src/types/nextjs-monorepo-workaround-plugin.d.ts
@@ -0,0 +1,4 @@
+declare module "@prisma/nextjs-monorepo-workaround-plugin" {
+  import { Plugin } from "webpack";
+  export class PrismaPlugin extends Plugin {}
+}
