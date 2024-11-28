353232c10895e80bb57ddb7ffa508c972abf3eda
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-11-28 11:11:54 -0400

    Export single instance in dev

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,39 +1,55 @@
 import logger from "@albion-raid-manager/logger";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-    {
-      emit: "event",
-      level: "error",
-    },
-    {
-      emit: "event",
-      level: "info",
-    },
-    {
-      emit: "event",
-      level: "warn",
-    },
-  ],
-});
+interface CustomNodeJsGlobal {
+  prisma: PrismaClient;
+}
 
-prisma.$on("query", (event) => {
-  logger.debug(`(Prisma) ${event.query}`, event);
-});
+declare const global: CustomNodeJsGlobal;
 
-prisma.$on("error", (event) => {
-  logger.error(`(Prisma) ${event.message}`, event);
-});
+const getPrismaClient = () => {
+  if (global.prisma) return global.prisma;
 
-prisma.$on("info", (event) => {
-  logger.info(`(Prisma) ${event.message}`, event);
-});
+  const prisma = new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "event",
+        level: "error",
+      },
+      {
+        emit: "event",
+        level: "info",
+      },
+      {
+        emit: "event",
+        level: "warn",
+      },
+    ],
+  });
 
-prisma.$on("warn", (event) => {
-  logger.warn(`(Prisma) ${event.message}`, event);
-});
+  prisma.$on("query", (event) => {
+    logger.debug(`(Prisma) ${event.query}`, event);
+  });
+
+  prisma.$on("error", (event) => {
+    logger.error(`(Prisma) ${event.message}`, event);
+  });
+
+  prisma.$on("info", (event) => {
+    logger.info(`(Prisma) ${event.message}`, event);
+  });
+
+  prisma.$on("warn", (event) => {
+    logger.warn(`(Prisma) ${event.message}`, event);
+  });
+
+  return prisma;
+};
+
+export const prisma = getPrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
