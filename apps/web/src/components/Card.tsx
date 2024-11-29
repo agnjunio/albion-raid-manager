57f8a063b69072d09d949728f522d61732a50f2c
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-11-29 17:11:54 -0400

    Use Card component on raid create

--- apps/web/src/components/Card.tsx
+++ apps/web/src/components/Card.tsx
@@ -4,17 +4,23 @@ export default function Card({
   title,
   actions,
   children,
+  className,
 }: {
   title?: string;
   actions?: React.ReactNode;
   children: React.ReactNode;
+  className?: string;
 }) {
+  const hasHeader = !!title || !!actions;
+
   return (
-    <div className="p-4 rounded-lg bg-primary-gray-800/25">
-      <div className="flex justify-between items-center mb-4">
-        {title && <h1 className="text-lg font-medium">{title}</h1>}
-        {actions && <div>{actions}</div>}
-      </div>
+    <div className={`p-4 rounded-lg bg-primary-gray-800/25 ${className}`}>
+      {hasHeader && (
+        <div className="flex justify-between items-center mb-4">
+          {title && <h1 className="text-lg font-medium">{title}</h1>}
+          {actions && <div>{actions}</div>}
+        </div>
+      )}
       {children}
     </div>
   );
