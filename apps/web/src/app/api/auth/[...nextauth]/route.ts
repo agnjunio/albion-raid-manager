b99f3d1d64988f5ecf63d2c1e391ab32f7234e05
Agnaldo Junior <agnaldo.junior01@gmail.com>
2024-12-01 07:22:38 -0400

    Add auth logger

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import config from "@albion-raid-manager/config";
+import logger from "@albion-raid-manager/logger";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import Discord from "next-auth/providers/discord";
 
@@ -18,6 +19,20 @@ export const nextAuthOptions: NextAuthOptions = {
       console.log(user.id);
       return session;
     },
+  },
+  logger: {
+    error(code, metadata) {
+      logger.error(code, metadata);
+    },
+    warn(code) {
+      logger.warn(code);
+    },
+    debug(code, metadata) {
+      logger.debug(code, metadata);
+    },
+  },
+  theme: {
+    colorScheme: "dark",
   },
 };
 
