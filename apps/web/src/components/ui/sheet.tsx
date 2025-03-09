09f175f232403e52fdc3fc074f800861d2b85ea4
Agnaldo Junior <agnaldo.junior01@gmail.com>
2025-03-09 03:32:32 -0400

    Some fixes in bg and components

--- apps/web/src/components/ui/sheet.tsx
+++ apps/web/src/components/ui/sheet.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { cn } from "@albion-raid-manager/common/helpers/classNames";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
-import { XIcon } from "lucide-react";
 import * as React from "react";
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
@@ -63,7 +64,7 @@ function SheetContent({
       >
         {children}
         <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          <XIcon className="size-4" />
+          <FontAwesomeIcon icon={faX} className="size-4" />
           <span className="sr-only">Close</span>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
