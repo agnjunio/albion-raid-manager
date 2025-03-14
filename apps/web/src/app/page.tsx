32478d322b44db3f701640c50f6e18fd05002970
Agnaldo Junior <agnaldo.junior01@gmail.com>
2025-03-14 03:26:53 -0400

    Add animations to logo

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -12,9 +12,16 @@ export default function Home() {
   const session = useSession();
 
   return (
-    <div className="z-1 flex size-full justify-center">
+    <div className="flex size-full justify-center">
       <div className="dark:bg-secondary/50 bg-secondary/40 relative z-10 basis-1/2 overflow-hidden bg-[url(/wallpapper.jpg)] bg-cover bg-[center_center] bg-no-repeat bg-blend-multiply lg:basis-1/3">
-        <div className="bg-radial -translate-1/2 from-primary/25 to-bg-primary/0 absolute left-2 top-2 size-[100vh] animate-pulse rounded-full to-100% blur-2xl [animation-duration:5000ms]" />
+        <div className="bg-radial -translate-1/2 from-primary/15 to-bg-primary/0 absolute -left-20 top-0 size-[150vh] animate-pulse rounded-full to-85% blur-2xl [animation-duration:1780ms] [animation-timing-function:ease-in-out]" />
+        <div className="bg-radial -translate-1/2 from-primary/15 to-bg-primary/0 absolute left-40 top-0 size-[80vh] animate-pulse rounded-full to-100% blur-2xl [animation-duration:2500ms]" />
+        <div className="bg-radial from-primary/15 to-bg-primary/0 absolute right-0 top-0 size-[40vh] -translate-y-2/3 animate-pulse rounded-full to-100% blur-2xl [animation-duration:1500ms]" />
+        <div className="bg-radial -translate-1/2 from-primary/10 to-bg-primary/0 absolute left-[15%] top-0 size-[50vh] animate-pulse rounded-full to-100% blur-2xl [animation-duration:1250ms]" />
+        <div className="bg-radial -translate-1/2 from-primary/10 to-bg-primary/0 absolute left-[35%] top-0 size-[60vh] animate-pulse rounded-full to-100% blur-2xl [animation-duration:1100ms]" />
+        <div className="bg-radial -translate-1/2 from-primary/10 to-bg-primary/0 absolute left-[65%] top-0 size-[55vh] animate-pulse rounded-full to-100% blur-2xl [animation-duration:1370ms]" />
+
+        <div className="bg-radial translate-1/2 from-secondary/50 to-secondary/15 absolute bottom-0 right-0 size-[80vh] animate-pulse rounded-full to-100% blur-2xl [animation-duration:5000ms]" />
 
         <div className="flex size-full flex-col items-center justify-center gap-2">
           <h1 className="text-secondary-foreground font-title text-5xl drop-shadow-lg">Albion Raid Manager</h1>
