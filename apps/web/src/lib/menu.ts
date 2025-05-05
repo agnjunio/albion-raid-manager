0eb6d3245a5d3c896ee3f5b77044f5bc2d96e3b1
Agnaldo Junior <agnaldo.junior01@gmail.com>
2025-05-05 00:56:30 -0400

    Remove members menu

--- apps/web/src/lib/menu.ts
+++ apps/web/src/lib/menu.ts
@@ -1,9 +1,8 @@
-import { faFlag, faGear, faPeopleGroup, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faFlag, faGear, faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
 
 export const links = [
   { href: "raids", label: "Raids", icon: faFlag },
   { href: "compositions", label: "Compositions", icon: faPeopleGroup },
-  { href: "members", label: "Members", icon: faUsers },
   {
     href: "settings",
     label: "Settings",
